863656b1cd50586a172288bd8f4650de463ea413
anais-lee <anaislee@seas.upenn.edu>
2015-12-29 01:18:34 -0500

    migrated manage.py

---
+++ myproject/fabfile.py
@@ -0,0 +1,17 @@
+from fabric.api import local
+
+def prepare_deployment(branch_name):
+    local('python manage.py test myapp')
+    local('git add -p && git commit')
+    local('git checkout master && git merge ' + branch_name)
+
+from fabric.api import lcd
+
+def deploy():
+    with lcd('/path/to/my/prod/area/'):
+        local('git pull /my/path/to/dev/area/')
+        local('python manage.py migrate myapp')
+        local('python manage.py test myapp')
+        local('/my/command/to/restart/webserver')
+
+
